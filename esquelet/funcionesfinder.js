7a2ae8256e231206e7b7817a78be6afd628904cc
Matsu94 <matuszdx@gmail.com>
2024-10-05 22:18:31 +0200

    Arreglado el eventListener acorde alfuncionamiento de getElementsByClassName

--- esquelet/funcionesfinder.js
+++ esquelet/funcionesfinder.js
@@ -23,12 +23,20 @@ function busqueda(notas, listaCanciones, resultadosEncontrados) {
     }
 }
 
-document.getElementsByClassName("key").addEventListener("click", function() {
-    notas.push(this.innerHTML);
-});
+let keys = document.getElementsByClassName("key");
+
+for (let i = 0; i < keys.length; i++) {
+    keys[i].addEventListener("click", function() {
+        let nota = this.querySelector(".key").innerHTML;
+        notas.push(nota);
+        busqueda(notas, listaCanciones, resultadosEncontrados);
+        document.getElementById("notas").innerHTML = notas.join(" ");
+        document.getElementById("resultats").innerHTML = resultadosEncontrados.join("<br>");
+    });
+}
 
 let listaCanciones = [
-            {cancion:"La Balanguera", notas:["DO", "RE", "MI", "FA", "FA", "SOL", "SOL", "LAS", "LAS"]},
+            {cancion:"La Balanguera", notas:["DO", "RE", "MI", "FA", "FA", "SOL", "SOL", "LA#", "LA#"]},
             {cancion:"Happy Birthday", notas:["DO", "DO", "RE", "DO", "FA", "MI", "DO", "DO", "RE", "DO", "SOL", "FA"]}
 ]
 
@@ -43,6 +51,10 @@ let listaCanciones = [
    CANCIONES, COMPARAMOS PRIMERA NOTA Y SI SALE COMPARAMOS LA SIGUIENTE 
    DE CADA UNO EN PLAN N - I DND N = NOTAS.LENGTH, ESTE PARECÍA MAS JODIDO
    ANTES DE ESCRIBIRLO 
+
+   JAER, CON LO TOCADA DE HVS QUE FUERON LOS BUCLES (Y CREO SE PODRÍAN MEJORAR)
+   ME HABRÍA QUEDADO CON LA PRIMER OPCION, HACER STRING, PASAR NOTAS DE CANCIONES 
+   A STRING TMB Y TIRARLE UN REGEX T.T 
         if (notas[0] = listaCanciones.notas[i]) {
             
             
